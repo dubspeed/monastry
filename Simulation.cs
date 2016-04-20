8dea8d6a886c2c2a37c454f154c15b4f8c5a5cc9
Michael Lennartz <michael.lennartz@innogames.de>
2016-04-20 08:13:40 +0200

    update

---
+++ Simulation.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Monastry 
+{
+	public class Simulation : MonoBehaviour 
+	{
+		public float SimulationTimeStepInSec;
+
+		public bool SimulationRunning = false;
+
+		private WorldController WorldController;
+		private int IterationCount;
+
+		void Start () 
+		{
+			WorldController = this.GetComponent<WorldController> ();
+			IterationCount = 0;
+			SimulationRunning = true;
+			SimulationTimeStepInSec = SimulationTimeStepInSec > 0.0f ? SimulationTimeStepInSec : 1.0f;
+			Invoke ("SimulationStep", SimulationTimeStepInSec);	
+		}
+
+		void SimulationStep() 
+		{
+			if (!SimulationRunning) {
+				return;
+			}
+			IterationCount += 1;
+
+			WorldController.SendCitizensToMass (IterationCount);
+			Invoke ("SimulationStep", SimulationTimeStepInSec);	
+		}
+			
+		void Update () 
+		{
+		
+		}
+	}
+}
--- WorldController.cs
+++ WorldController.cs
@@ -1,53 +1,89 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 using UnityEngine.Assertions;
 
 namespace Monastry
 {
+	[System.Serializable]
 	public class MassVisit
 	{
 		// Was the visitor blessed, or did he have to wait too long and went away
-		public bool GotBlessed { get; set; }
+		public bool GotBlessed;
 
-		// was he impressed by the temple?
-		public float WasImpressed { get; set; }
+		// how much was the visitor impressed by the temple?
+		[Range(0.0f, 1.0f)]
+		public float Impression;
 
 		// what did he pay
-		public float PayedAmount { get; set; }
+		[Range(0, 10)] 
+		public int PayedAmount;
 
-		public float TotalScore()
+		public MassVisit(bool gotBlessed, float impression)
 		{
-			return 0.0f;
+			GotBlessed = gotBlessed;
+			Impression = impression;
+		}
+
+		// total score of the visit
+		// ranges from -1.0f to 1.4f;
+		public float GetTotalScore()
+		{
+			float blessFactor = GotBlessed ? 0.4f : 0.2f;
+			return (Impression + blessFactor - 0.7f) * 2.0f;
 		}
 	}
 
+	[System.Serializable]
 	public class Citizen
 	{
-		public int BaseAttitude { get; set; }
+		[Range(0.0f, 100.0f)]
+		public float BaseAttitude;
+		public List<MassVisit> MassesVisited;
 
-		public MassVisit[] MassesVisited { get; set; }
+		public Citizen(float baseAttitude)
+		{
+			BaseAttitude = baseAttitude;
+			MassesVisited = new List<MassVisit> ();
+		}
 
+		public void GoToMass()
+		{
+			MassVisit visit = new MassVisit (true, 0.6f);
+			MassesVisited.Add (visit);
+
+			BaseAttitude += visit.GetTotalScore ();
+		}
 	}
 
 	public class WorldController : MonoBehaviour
 	{
-
-		public int NumberOfCitizen;
+		public int PopulationCount;
+		public List<Citizen> Citizens;
 
 		// Use this for initialization
 		void Start ()
 		{
-			Assert.IsTrue (NumberOfCitizen > 0, "WorldController: NumberOFCitizen is 0");
+			PopulationCount = PopulationCount > 0 ? PopulationCount : 100;
 
-			// create x citizen
-
+			for (int i = 0; i < PopulationCount; i++) {
+				Citizen citizen = new Citizen (20.0f);
+				Citizens.Add (citizen);
+				citizen.GoToMass ();
+			}
 		}
 
-		// Update is called once per frame
-		void Update ()
-		{
+		// Randomly send some citizen to mass
+		public int SendCitizensToMass(int iterationCount) {
+			float one_percent = PopulationCount / 100.0f;
 
+			// Every step 1% +- 0.3% of the population goes to mass
+			one_percent += Random.Range(-1 * one_percent * 0.3f, one_percent * 0.3f); 
+
+			int people_send = Mathf.RoundToInt (one_percent);
+			print ("day " + iterationCount + " -> People send to mass: " + people_send);
+
+			return people_send;
 		}
 	}
 }
